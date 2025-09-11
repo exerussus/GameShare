d2ad8e475054b699671bb046253f1440c10d422d
Exerussus <solovyov.production@gmail.com>
2025-09-11 18:48:24 +0300

    init commit

---
+++ Runtime/Abstractions.cs
@@ -0,0 +1,12 @@
+﻿namespace Exerussus.GameSharing.Runtime
+{    
+    public interface IInjectable
+    {
+        public void Inject(GameShare gameShare);
+    }
+    
+    public interface IGameSharable
+    {
+        public void ShareWith(GameShare gameShare);
+    }
+}
